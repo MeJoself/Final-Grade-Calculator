fbf041823d50562c381c2984c2c99024461ee77f
Jose Faustino <josefaustino.channel@gmail.com>
2022-03-01 14:15:11 -0700

    MVP

--- Final Grade Calculator/ViewController.swift
+++ Final Grade Calculator/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func bottonClick(_ sender: Any)
+    {
+        self.view.backgroundColor = UIColor.systemRed
+        myLabel.text = "Current Grade: \()"
+    }
+    
 }
 
