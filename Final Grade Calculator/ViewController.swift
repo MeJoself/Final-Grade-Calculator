7e17ecf493e7ad838c75260e3fc8b7b31f7d5ab2
Jose Faustino <josefaustino.channel@gmail.com>
2022-03-02 14:13:20 -0700

    MVP

--- Final Grade Calculator/ViewController.swift
+++ Final Grade Calculator/ViewController.swift
@@ -9,16 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    @IBOutlet weak var otherLabel: UILabel!
     @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var textTwo: UITextField!
+    
+    @IBOutlet weak var finalNumber: UILabel!
+    @IBOutlet weak var textThree: UITextField!
+    @IBOutlet weak var textOne: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func bottonClick(_ sender: Any)
+    
+    @IBAction func mathEasyMode(_ sender: Any)
     {
+        var currentGrade = textOne.text ?? "87"
+        var desiredGrade = textTwo.text ?? "95"
+        var finalExam = textThree.text ?? "20"
+        var current = Int(currentGrade) ?? 87
+        var desire = Int(desiredGrade) ?? 95
+        var final = Int(finalExam) ?? 20
+        var F = (desire - ((100 - final) * current)) / final
+        print(F)
         self.view.backgroundColor = UIColor.systemRed
-        myLabel.text = "Current Grade: \()"
+        finalNumber.text = "Grade Needed: \(F)"
     }
     
 }
